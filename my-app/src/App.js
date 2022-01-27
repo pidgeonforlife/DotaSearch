4855bb659b2b219e7a3138d726d5a19a4e996faf
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-01-26 19:11:19 -0800

    Implemented function that saved the player data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const [searchText, setSearchText] = useState("");
-  const API_KEY = "928BF6B809AF9C4C09EC4B6E28A030D9";
+  const [playerData, setPlayerData] = useState({});
+  const API_KEY = "212d088e-95ed-45a9-a37f-14e4ad6bdb29";
+  
+  function searchForPlayer(event) {
+    //Set up the correct API call
+    var APICallString = "https://api.opendota.com/api/players/" + searchText + "?api_key=" + API_KEY;
+    //Handle the API call
+    axios.get(APICallString).then(function (response) {
+    //Success
+      setPlayerData(response.data);
+    }).catch(function (error) {
+    //Error
+      console.log(error);
+    console.log(error);
+    });
+  }
 
 
   return (
@@ -12,7 +28,7 @@ function App() {
       <div className='container'>
         <h5>Dota 2 Player Search</h5>
         <input type='text' onChange={e => setSearchText(e.target.value)}></input>
-        <button>Search for Player</button>
+        <button onClick={e => searchForPlayer(e)}>Search for Player</button>
       </div>
     </div>
   );
